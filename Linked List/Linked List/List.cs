5063ea8ba5ea9ad8d5d138bdf89f5225f07e0e9c
markdgzman <mark.dgzman@gmail.com>
2014-04-18 12:35:47 -0700

    Linked List
    Started implementation of a Linked List using C#

---
+++ Linked List/Linked List/List.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    class List
+    {
+        public class Node
+        {
+            public String name;
+            public Node next;
+        }
+
+        public class LinkedList
+        {
+            private Node head;
+
+            public void insert(String data)
+            {
+                Node add = new Node();
+                add.name = data;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Node a = new Node();
+        }
+    }
+}
---
+++ Linked List/Linked List/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List prog = new List();
+            Console.WriteLine(prog.isEmpty());
+
+            prog.Append();
+            prog.Append();
+            prog.Append();
+            Console.ReadKey();
+        }
+    }
+}
