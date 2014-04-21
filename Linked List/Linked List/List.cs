499c387989db48713adefaee72cdcec08c97a59d
markdgzman <mark.dgzman@gmail.com>
2014-04-21 00:02:31 -0700

    More Comment Logic
    Deleted program.cs

--- Linked List/Linked List/List.cs
+++ Linked List/Linked List/List.cs
@@ -16,18 +16,27 @@ namespace Linked_List
 
         public class LinkedList
         {
+
             private Node head;
+            //Add another note data type to point towards next
+
 
             public void insert(String data)
             {
                 Node add = new Node();
                 add.name = data;
+
+                //check if head node is empty
+                //else add to next
+                
             }
         }
 
         static void Main(string[] args)
         {
             Node a = new Node();
+
+            //insert to linked list
         }
     }
 }
