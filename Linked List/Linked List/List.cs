4a5afcc40c513ec03f39ab958aeaf6e53824e2e2
markdgzman <mark.dgzman@gmail.com>
2014-04-21 21:30:56 -0700

    Method Updates
    Added constructors for node class. Also added insert and display methods
    for Linked List. Program inserting values in backwards.

--- Linked List/Linked List/List.cs
+++ Linked List/Linked List/List.cs
@@ -12,30 +12,91 @@ namespace Linked_List
         {
             public String name;
             public Node next;
+
+            public Node()
+            {
+                name = null;
+                next = null;
+            }
+            
+            public Node(String v, Node s)
+            {
+                name = v;
+                next = s;
+            }
+
+            public Node(String v)
+            {
+                name = v;
+            }
         }
 
         public class LinkedList
         {
-
             private Node head;
+            /*
+            private Node head;
+            private Node next;*/
             //Add another note data type to point towards next
 
+            public LinkedList()
+            {
+                head = new Node();
+            }
 
             public void insert(String data)
             {
-                Node add = new Node();
-                add.name = data;
+                Node newNode = new Node();
+                if (head == null)
+                    head.name = data;
+                else
+                {
+                    newNode.name = data;
+                    newNode.next = head;
+                    head = newNode;
+                }  
+            }
 
-                //check if head node is empty
-                //else add to next
+            public void display()
+            {
                 
+                while(head != null)
+                {
+                    Console.WriteLine(head.name);
+                    head = head.next;
+                }
             }
         }
 
         static void Main(string[] args)
         {
-            Node a = new Node();
 
+            LinkedList a = new LinkedList();
+            a.insert("Boo");
+            a.insert("Plaster");
+            a.insert("Yourself");
+            a.display();
+
+            Node b = new Node("Ottawa");
+            Node c = new Node("Canada",b);
+            Node d = new Node("Hello",c);
+            Node e = new Node("Good",d);
+/*
+            b.name = "Ottawa";
+            b.next = c;
+            c.name = "Canada";
+            c.next = d;
+            d.name = "Hello";
+            d.next = e;
+            e.name = "Good";*/
+            /*a.insert("Ottawa");
+            a.insert("Canada");
+            a.insert("Hey");
+            a.insert("Hello");
+            a.insert("Bye");
+            a.insert("Good");
+            */
+            Console.Read();
             //insert to linked list
         }
     }
