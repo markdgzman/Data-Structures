68170eb8e4ed4cc30a78c2bd9a4d07ddfa0776f3
unknown <cs446001_ta2@CS93960.cs2k3.cwu.edu>
2014-05-08 09:51:09 -0700

    Updated working Linked List.

---
+++ Linked List/Linked List/LinkedList.cs
@@ -0,0 +1,75 @@
+﻿namespace Linked_List
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    /// 
+
+    public class Node
+    {
+        public object data;
+        public Node next = null;
+
+        public Node(object d)
+        {
+            data = d;
+        }
+
+    }
+
+    public class LinkedList
+    {
+        Node list;
+
+        public void insert(object d)
+        {
+            if (list == null)
+            {
+                list = new Node(d);
+                return;
+            }
+
+            Node newNode = new Node(d);
+            Node current = list;
+
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+
+        public void delete(int index)
+        {
+            Node bIndex = list;
+            Node aIndex = list;
+
+            for (int i = 1; i < index - 1; i++)
+                bIndex = bIndex.next;
+
+            for (int i = 1; i < index + 1; i++)
+                aIndex = aIndex.next;
+
+            bIndex.next = aIndex;
+        }
+
+        public void toString()
+        {
+            Node current = list;
+            int indexCounter = 1;
+            while (current.next != null)
+            {
+                Console.WriteLine("Node " + indexCounter + ": " + current.data);
+                current = current.next;
+                indexCounter++;
+            }
+            Console.WriteLine("Node " + indexCounter + ": " + current.data);
+        }
+    }
+}
--- Linked List/Linked List/List.cs
+++ Linked List/Linked List/List.cs
@@ -8,97 +8,40 @@ namespace Linked_List
 {
     class List
     {
-        public class Node
-        {
-            public String name;
-            public Node next;
-
-            public Node()
-            {
-                name = null;
-                next = null;
-            }
-            
-            public Node(String v, Node s)
-            {
-                name = v;
-                next = s;
-            }
-
-            public Node(String v)
-            {
-                name = v;
-            }
-        }
-
-        public class LinkedList
-        {
-            private Node head;
-            /*
-            private Node head;
-            private Node next;*/
-            //Add another note data type to point towards next
-
-            public LinkedList()
-            {
-                head = new Node();
-            }
-
-            public void insert(String data)
-            {
-                Node newNode = new Node();
-                if (head == null)
-                    head.name = data;
-                else
-                {
-                    newNode.name = data;
-                    newNode.next = head;
-                    head = newNode;
-                }  
-            }
-
-            public void display()
-            {
-                
-                while(head != null)
-                {
-                    Console.WriteLine(head.name);
-                    head = head.next;
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
 
             LinkedList a = new LinkedList();
             a.insert("Boo");
+            a.insert(5);
+            a.insert(60);
+            a.insert(2);
             a.insert("Plaster");
             a.insert("Yourself");
             a.insert("Canada");
-            a.display();
-
-            Node b = new Node("Ottawa");
-            Node c = new Node("Canada",b);
-            Node d = new Node("Hello",c);
-            Node e = new Node("Good",d);
-/*
-            b.name = "Ottawa";
-            b.next = c;
-            c.name = "Canada";
-            c.next = d;
-            d.name = "Hello";
-            d.next = e;
-            e.name = "Good";*/
-            /*a.insert("Ottawa");
+            a.insert(5);
+            a.insert(60);
+            a.insert(2);
+            a.insert(10);
+            a.insert(4);
+            a.insert("Yourself");
             a.insert("Canada");
-            a.insert("Hey");
-            a.insert("Hello");
-            a.insert("Bye");
-            a.insert("Good");
-            */
+            a.insert(20);
+            a.insert(1);
+            a.insert(17);
+
+            a.toString();
+
+            a.delete(3);
+
+            Console.WriteLine("\r\nDeleting Node index 3");
+            a.toString();
+
+            Console.WriteLine("\r\nDeleting Node index 3 and 6");
+            a.delete(3);
+            a.delete(6);
+            a.toString();
             Console.Read();
-            //insert to linked list
         }
     }
 }
